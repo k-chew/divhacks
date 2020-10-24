f0245ea1d76d5189ddd92ecd78c5760fda62a80a
k-chew <kailey.chew@uwaterloo.ca>
2020-10-24 19:05:23 -0400

    Basic web app with navbars with Google calendar authorization

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>HELLO<h1>"
+    return render_template('home.html')
+
+@app.route("/cal")
+def calendar():
+    return render_template('calendar.html')
+
+@app.route("/children")
+def children():
+    return render_template('children.html')
+
+@app.route("/rewards")
+def rewards():
+    return render_template('rewards.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='localhost', port='8000')
